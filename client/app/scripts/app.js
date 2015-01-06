f7cd5d545f0ebf1aab59922ae6735ebe80182e4c
Brian <blumpk@gmail.com>
2015-01-05 20:16:51 -0800

    Updated Fantasy App Files

--- client/app/scripts/app.js
+++ client/app/scripts/app.js
@@ -9,7 +9,7 @@
  * Main module of the application.
  */
 angular
-  .module('clientApp', [
+  .module('fantasyApp', [
     'ngAnimate',
     'ngCookies',
     'ngResource',
@@ -20,8 +20,8 @@ angular
   .config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
+        templateUrl: 'views/home.html',
+        controller: 'HomeCtrl'
       })
       .when('/about', {
         templateUrl: 'views/about.html',
