ad25745d06a0f935f26e0c4887f9e1f4df876a9b
brian <blumpk@gmail.com>
2015-02-27 23:14:12 -0800

    Updated Fantasy App Files

--- server/db.js
+++ server/db.js
@@ -2,5 +2,5 @@
  * Created by brian on 1/25/15.
  */
 module.exports = {
-    'url': 'mongodb://localhost/nba'
+    'url': 'mongodb://blumpk:pacers721@ds049641.mongolab.com:49641/fanalytics_zone'
 }
