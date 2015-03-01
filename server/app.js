2c2e7b41ca9d0c474dc1f6f015fdaec79924743d
Brian <blumpk@gmail.com>
2015-02-28 21:44:00 -0800

    Updated Fantasy App Files

--- server/app.js
+++ server/app.js
@@ -15,6 +15,8 @@ mongoose.connect(dbConfig.url);
 var passport = require('passport');
 var expressSession = require('express-session');
 
+
+
 app.use(expressSession({secret: 'mySecretKey'}));
 app.use(passport.initialize());
 app.use(passport.session());
